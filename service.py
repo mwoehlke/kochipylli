0a6f8cf4c4a127166e045cfcc823837b8745b451
Matthew Woehlke <mw_triad@users.sourceforge.net>
2013-07-01 23:43:21 -0400

    Implement database
    Implement reading of database and existing files in archive when binding
    a service. Add method to write to database.

--- service.py
+++ service.py
@@ -9,21 +9,79 @@ from PyQt4.QtGui import *
 from PyQt4.QtNetwork import *
 
 #==============================================================================
+def listFiles(out, path):
+    # Get files
+    for entry in path.entryList(QDir.Files):
+        full_path = QDir(path.path() + "/" + entry).canonicalPath()
+        if entry in out:
+            out[entry] += [full_path]
+        else:
+            out[entry] = [full_path]
+
+    # Recurse through directories
+    for entry in path.entryList(QDir.Dirs | QDir.NoDotAndDotDot):
+        listFiles(out, QDir(path.path() + "/" + entry))
+
+#==============================================================================
 class Service(QObject):
+    #--------------------------------------------------------------------------
+    # Initialization
     #--------------------------------------------------------------------------
     def __init__(self, parent = None):
         QObject.__init__(self, parent)
         self.m_window = None
         self.m_archive = None
+        self.m_database = None
         self.m_net_manager = QNetworkAccessManager(self)
+
+        self.m_existing_files = {}
+        self.m_results = {}
+        self.m_database_entries = {}
 
     #--------------------------------------------------------------------------
     def bind(self, window, archive):
         self.m_window = window
         self.m_archive = QDir(archive)
+        self.m_database_dir = QDir(archive.path() + "/.kochipylli/database")
+        self.m_results_dir = QDir(archive.path() + "/.kochipylli/results")
+
+        archive.mkpath(self.m_database_dir.path())
+        archive.mkpath(self.m_results_dir.path())
+
+        listFiles(self.m_existing_files, archive)
+        self.readDatabase()
 
     #--------------------------------------------------------------------------
-    def requestImageListing(self, url):
+    # Database Interaction
+    #--------------------------------------------------------------------------
+    def readDatabase(self):
+        self.m_database = QFile(self.m_database_dir.canonicalPath() + "/index")
+        if not self.m_database.open(QIODevice.ReadWrite | QIODevice.Append):
+            raise IOError(i18n("Unable to open database index"))
+
+        self.m_database.reset()
+        while not self.m_database.atEnd():
+            entry = self.m_database.readLine()
+            if not entry.isEmpty():
+                entry = QString.fromUtf8(entry.left(entry.length() - 1))
+                self.parseDatabaseEntry(entry)
+
+    #--------------------------------------------------------------------------
+    def parseDatabaseEntry(self, entry):
+        n = entry.indexOf("=")
+        if n >= 0:
+            self.m_database_entries[entry.left(n)] = entry.mid(n + 1)
+
+    #--------------------------------------------------------------------------
+    def writeDatabaseEntry(self, key, value):
+        self.m_database_entries[key] = value
+        line = QString("%1=%2\n").arg(key, value)
+        r = self.m_database.write(line.toUtf8())
+        self.m_database.flush()
+
+    #--------------------------------------------------------------------------
+    # Web Interaction
+    #--------------------------------------------------------------------------
         reply = self.m_net_manager.get(QNetworkRequest(QUrl(url)))
         reply.error.connect(reply.deleteLater)
         reply.finished.connect(self.dispatchImageListingRequest)
